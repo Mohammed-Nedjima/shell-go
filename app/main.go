d7850237a9c840d28cdcf7f6a22b9623ad3c0f88
Mohammed-Nedjima <ayoubnedjma09@gmail.com>
2026-02-17 22:49:19 +0100

    refactorin the replay functionality by adding supportedCommands map and replay function

--- app/main.go
+++ app/main.go
@@ -4,20 +4,36 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
+func replay(supportedCommands map[string]func(args ...string)) {
+	fmt.Print("$ ")
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	command = command[:len(command)-1]
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		os.Exit(1)
+	}
+	args := strings.Split(strings.TrimSpace(command), " ")
+
+	if len(args) == 0 {
+		return
+	}
+	excutorFunction, ok := supportedCommands[args[0]]
+	if !ok {
+		fmt.Printf("%s: command not found\n", command)
+		return
+	}
+	excutorFunction(args...)
+}
+
+func handleCommand(command string) {
+}
 func main() {
+	supportedCommands := make(map[string]func(args ...string))
+	supportedCommands["exit"] = func(args ...string) { os.Exit(0) }
 	for {
-		fmt.Print("$ ")
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error reading input:", err)
-			os.Exit(1)
-		}
-		if command[:len(command)-1] == "exit" {
-			return
-		} else {
-			fmt.Println(command[:len(command)-1] + ": command not found")
-		}
+		replay(supportedCommands)
 	}
 }
