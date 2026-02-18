1cf9a789a7e99ef10014245ba1b9bcc4c49eff65
Mohammed-Nedjima <ayoubnedjma09@gmail.com>
2026-02-18 11:57:20 +0100

    adding the type command and refactorin g

--- app/main.go
+++ app/main.go
@@ -7,9 +7,13 @@ import (
 	"strings"
 )
 
-func replay(supportedCommands map[string]func(args ...string)) {
+type commandMap map[string]func(args ...string)
+
+var supportedCommands commandMap
+
+func replay(reader *bufio.Reader) {
 	fmt.Print("$ ")
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	command, err := reader.ReadString('\n')
 	command = command[:len(command)-1]
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error reading input:", err)
@@ -20,20 +24,31 @@ func replay(supportedCommands map[string]func(args ...string)) {
 	if args[0] == "" {
 		return
 	}
-	excutorFunction, ok := supportedCommands[args[0]]
+	executorFunction, ok := supportedCommands[args[0]]
 	if !ok {
 		fmt.Printf("%s: command not found\n", command)
 		return
 	}
-	excutorFunction(args[1:]...)
+	executorFunction(args[1:]...)
 }
 
 func main() {
-	supportedCommands := map[string]func(args ...string){
+	supportedCommands = commandMap{
 		"exit": func(args ...string) { os.Exit(0) },
 		"echo": func(args ...string) { fmt.Printf("%s\n", strings.Join(args, " ")) },
+		"type": func(args ...string) {
+			for _, arg := range args {
+				_, ok := supportedCommands[arg]
+				if !ok {
+					fmt.Printf("%s: command not found\n", arg)
+					continue
+				}
+				fmt.Printf("%s is a shell builtin\n", arg)
+			}
+		},
 	}
+	reader := bufio.NewReader(os.Stdin)
 	for {
-		replay(supportedCommands)
+		replay(reader)
 	}
 }
